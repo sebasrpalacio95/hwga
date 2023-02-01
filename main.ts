9c62cefb68d3dbb98ea4b30130acaec641d0061d
sebasrpalacio95 <sebastianrobledo95@gmail.com>
2023-02-01 11:52:58 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,57 @@
+/**
+ * The SHT2x Driver provides access to the functionality of the SHT20, SHT21 
+ * and SHT2x sensors
+*/
+
+let sht_i2c_address = 0x40;
+
+//% color=#4c6ef5 weight=25 icon="\uf043" block="SHT2x Sensor"
+namespace SHT2xDriver {
+    /**
+     * Read Relative Humidity from the SHT2x Sensor.
+     * Returns a number describing the relative humidity in percentage % relative
+     * humidity
+    */
+    //% blockId="SHT2xDriver_read_humidity"
+    //% block="read humidity"
+    export function read_humidity(): number {
+        pins.i2cWriteNumber(sht_i2c_address, 0x2C, NumberFormat.UInt8LE, false);
+        basic.pause(100);
+        let buff = pins.i2cReadBuffer(sht_i2c_address, 3);
+        let result = buff[0] << 8;
+        result |= buff[1];
+        result = (((625 * result) >> 12)) / 100;
+        return result;
+    }
+
+    /**
+     * Read Temperature in degrees celcius from the SHT2x sensor.
+     * Returns a number describing the ambient temperature in degrees celsius
+    */
+    //% blockId="SHT2xDriver_read_temperature"
+    //% block="read temperature"
+    export function read_temperature(): number {
+        pins.i2cWriteNumber(sht_i2c_address, 0x2C, NumberFormat.UInt8LE, false);
+        basic.pause(100);
+        let buff = pins.i2cReadBuffer(sht_i2c_address, 3);
+        let result = buff[0] << 8;
+        result |= buff[1];
+        result = (((4375 * result) >> 14) - 4500) / 100;
+        return result;
+    }
+
+    /** 
+     * Change the I2c address used to interact with the SHT2x to the given
+     * address. The I2c address should be within 0-127.
+    */
+    //% blockId="SHT2xDriver_set_i2c_address"
+    //% block="Change i2c address to %address"
+    //% advanced=true
+    export function set_i2c_address(address: number) {
+        if (address < 0 || address > 128) basic.showString("Invalid I2C address");
+        else sht_i2c_address = address;
+    }
+}
+basic.forever(function () {
+	
+})
